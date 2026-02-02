81b69f831a328d191031c881e383c548d754f22c
Youness CN8VX <cn8vx.ma@gmail.com>
2026-02-02 18:41:45 +0100

    Delete include directory

--- include/tgdb.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$tgdb_array = [
-    '604' => 'Talk Group MAROC - National',
-    '6041' => 'TG Cross Mode DMR<=>ANALOGUIQUE',
-    '60401' => 'Talk Group officiel du serveur FreeDMR_Maroc',
-    '604112' => 'EmComm Morocco',
-] ;
-?>
